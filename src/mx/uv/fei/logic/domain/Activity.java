93c929f7d2cd1ccda07afdc2e677256bcbb9c301
Luigi573 <106448901+Luigi573@users.noreply.github.com>
2023-04-29 07:45:42 -0600

    Add files via upload

--- src/mx/uv/fei/logic/domain/Activity.java
+++ src/mx/uv/fei/logic/domain/Activity.java
@@ -1,18 +1,26 @@
 package mx.uv.fei.logic.domain;
 
-import java.text.SimpleDateFormat;
+import java.sql.Date;
 
 public class Activity {
-    String title;
-    String description;
-    SimpleDateFormat startDate;
-    SimpleDateFormat dueDate;
-    
-    public Activity(String title, String description, SimpleDateFormat startDate, SimpleDateFormat dueDate){
+    private int id;
+    private String title;
+    private String description;
+    private Date startDate;
+    private Date dueDate;
+   
+    public Activity(){
+        id = 0;
+    }
+    public Activity(String title, String description, Date startDate, Date dueDate){
+        this();
         this.title = title;
         this.description = description;
         this.startDate = startDate;
         this.dueDate = dueDate;
+    }
+    public void setId(int id){
+        this.id = id;
     }
     public void setTitle(String title){
         this.title = title;
@@ -20,11 +28,14 @@ public class Activity {
     public void setDescription(String description){
         this.description = description;
     }
-    public void setStartDate(SimpleDateFormat startDate){
+    public void setStartDate(Date startDate){
         this.startDate = startDate;
     }
-    public void setDueDate(SimpleDateFormat dueDate){
+    public void setDueDate(Date dueDate){
         this.dueDate = dueDate;
+    }
+    public int getId(){
+        return id;
     }
     public String getTitle(){
         return title;
@@ -32,10 +43,14 @@ public class Activity {
     public String getDescription(){
         return description;
     }
-    public SimpleDateFormat getStartDate(){
+    public Date getStartDate(){
         return startDate;
     }
-    public SimpleDateFormat getDueDate(){
+    public Date getDueDate(){
         return dueDate;
+    }
+    @Override
+    public String toString(){
+        return "Title: " + title + "\nDescription: " + description + "\nStartDate: " + startDate + "\nDueDate: " + dueDate;
     }
 }
