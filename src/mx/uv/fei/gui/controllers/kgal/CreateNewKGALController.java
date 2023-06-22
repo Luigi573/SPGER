c8db3848dc857fe074ee7a61d6212b0ea62ef9e9
Luigi573 <xavier.arian@gmail.com>
2023-06-22 10:14:55 -0600

    KGALManager
    Fully integrated KGAL manager and fixed some ugly code

--- src/mx/uv/fei/gui/controllers/kgal/CreateNewKGALController.java
+++ src/mx/uv/fei/gui/controllers/kgal/CreateNewKGALController.java
@@ -9,51 +9,82 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import mx.uv.fei.gui.AlertPopUpGenerator;
+import mx.uv.fei.gui.controllers.HeaderPaneController;
+import mx.uv.fei.gui.controllers.MainMenuController;
 import mx.uv.fei.logic.daos.KGALDAO;
 import mx.uv.fei.logic.domain.KGAL;
+import mx.uv.fei.logic.domain.User;
 import mx.uv.fei.logic.exceptions.DataInsertionException;
 
 public class CreateNewKGALController {
+    private User user;
     
+    @FXML
+    private Pane headerPane;
     @FXML
     private TextField tfDescription;
 
     @FXML
     public void createNewKGAL(ActionEvent event) {
-        String newKGALDescription = tfDescription.getText();
-        int result = 0;
-        KGALDAO kgalDAO = new KGALDAO();
         KGAL newKgal = new KGAL();
-        newKgal.setDescription(newKGALDescription);
+        newKgal.setDescription(tfDescription.getText());
+        
         try {
-            result = kgalDAO.addKGAL(newKgal);
+            KGALDAO kgalDAO = new KGALDAO();
+            
+            if(kgalDAO.addKGAL(newKgal) > 0) {
+                switchToKGALListScene(event);
+                
+                new AlertPopUpGenerator().showCustomMessage(Alert.AlertType.CONFIRMATION, "Operación exitosa", "Se ha guardado la nueva LGAC correctamente.");
+            }
         } catch (DataInsertionException exception) {
             new AlertPopUpGenerator().showConnectionErrorMessage();
         }
-        if(result > 0) {
-            Alert successMessage = new Alert(Alert.AlertType.CONFIRMATION);
-            successMessage.setHeaderText("Operación exitosa");
-            successMessage.setContentText("Se ha guardado la nueva LGAC correctamente.");
-            successMessage.showAndWait();
-        }
+        
         tfDescription.clear();
     }
 
     @FXML
     public void switchToKGALListScene(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mx/uv/fei/gui/fxml/kgal/KGALList.fxml"));
+        
         try {
-
-            Parent root = FXMLLoader.load(getClass().getResource("/mx/uv/fei/gui/fxml/kgal/KGALList.fxml"));
-            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            Scene scene = new Scene(root);
+            Parent root = loader.load();
+            KGALListController controller = (KGALListController)loader.getController();
+            controller.setUser(user);
+            controller.loadHeader();
+            controller.loadKgalList();
             
+            Scene scene = new Scene(root);
+            String css = this.getClass().getResource("/mx/uv/fei/gui/stylesfiles/Styles.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
             new AlertPopUpGenerator().showMissingFilesMessage();
         }
     }
-
+    
+    public void loadHeader(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mx/uv/fei/gui/fxml/HeaderPane.fxml"));
+        
+        try{
+            Pane header = loader.load();
+            HeaderPaneController controller = (HeaderPaneController)loader.getController();
+            controller.setUser(user);
+            
+            headerPane.getChildren().setAll(header);
+        }catch(IOException exception){
+            new AlertPopUpGenerator().showMissingFilesMessage();
+        }
+    }
+    
+    public void setUser(User user){
+        this.user = user;
+    }
 }
